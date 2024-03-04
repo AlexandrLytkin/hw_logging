a24114cb198d261d4922155e0639cd12425785fe
AlexandrLytkin <Dracca@mail.ru>
2024-03-04 13:00:26 +0300

    Сделал клон проекта копировал к себе, загрузил requirements.txt, добавил ветку

--- RequestLogger/main.py
+++ RequestLogger/main.py
@@ -4,7 +4,6 @@ import logging
 logger = logging.getLogger('RequestsLogger')
 
 
-
 sites = ['https://www.youtube.com/', 'https://instagram.com', 'https://wikipedia.org', 'https://yahoo.com',
          'https://yandex.ru', 'https://whatsapp.com', 'https://twitter.com', 'https://amazon.com', 'https://tiktok.com',
          'https://www.ozon.ru']
